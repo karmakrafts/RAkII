a725d8c1c9fb24d5003d69ffba859fb859af7223
KitsuneAlex <alexh@karmakrafts.dev>
2025-03-18 20:42:05 +0100

    Initial project setup, move to dev.karmakrafts group

---
+++ rakii/src/jvmMain/kotlin/dev/karmakrafts/rakii/Droppable.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2025 Karma Krafts & associates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.karmakrafts.rakii
+
+import java.lang.ref.Cleaner
+import java.lang.ref.WeakReference
+
+private val cleaner: Cleaner by lazy {
+    Cleaner.create { runnable ->
+        Thread(runnable).apply { name = "RAkII Cleaner" }
+    }
+}
+
+@PublishedApi
+internal actual fun registerDroppable(instance: Droppable) {
+    val ref = WeakReference(instance) // Make sure we don't directly capture instance
+    cleaner.register(instance) {
+        ref.get()?.drop()
+    }
+}
